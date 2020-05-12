788d3fcf8e1a236adfdadaf353396a05efe5eaa5
Artandrey <artandrey7777@gmail.com>
2020-05-12 16:05:30 +0300

    'fast-commit'

--- alert-scripts/alert-script.js
+++ alert-scripts/alert-script.js
@@ -1,11 +1,7 @@
 const messageCode = process.argv[2];
-
+const printSuccess = function (text) {
+    console.log(`\x1b[37;42;1m${text}\x1b[0m`);
+}
 const messages = {
-    'pusk-success': console.log(+ `
-    ╔══╦╗╔╦══╦══╦═══╦══╦══╦╗\n
-    ║╔═╣║║║╔═╣╔═╣╔══╣╔═╣╔═╣║\n
-    ║╚═╣║║║║─║║─║╚══╣╚═╣╚═╣║\n
-    ╚═╗║║║║║─║║─║╔══╩═╗╠═╗╠╝\n
-    ╔═╝║╚╝║╚═╣╚═╣╚══╦═╝╠═╝╠╗\n
-    ╚══╩══╩══╩══╩═══╩══╩══╩╝\n`)
+    'pusk-success': printSuccess('СКРИПТ УСПЕШНО ЗАКОНЧИЛ СВОЮ РАБОТУ'),
 }
