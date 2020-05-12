e892ff681dba8a84586b184a8fad766050a22e78
Artandrey <artandrey7777@gmail.com>
2020-05-12 19:45:05 +0300

    'fast-commit'

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// button js
+
+const wrapperMenu = document.querySelector('.wrapper-menu');
+
+wrapperMenu.addEventListener('click', function(){
+  wrapperMenu.classList.toggle('open');  
+})
