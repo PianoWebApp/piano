646552002dcc5f0c0719168a3396302a1ae51698
Artandrey <artandrey7777@gmail.com>
2020-05-12 15:38:35 +0300

    'fast-commit'

--- alert-scripts/alert-script.js
+++ alert-scripts/alert-script.js
@@ -1,5 +1,5 @@
 const messageCode = process.argv[2];
 
 const messages = {
-    'pusk-success': console.log('%c СКРИПТ ВЫГРУЗКИ НА ГИТ ЗАКОНЧИЛ СВОЮ РАБОТУ')
+    'pusk-success': console.log('%c' + 'СКРИПТ ВЫГРУЗКИ НА ГИТ ЗАКОНЧИЛ СВОЮ РАБОТУ', 'padding: 10px; background-color: #43a047; color: #f5f5f5')
 }
