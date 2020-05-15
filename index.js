d0b924d1e2ddbd81b55c089f90399db1dc3f031b
Artandrey <artandrey7777@gmail.com>
2020-05-15 23:35:59 +0300

    'fast-commit'

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 'use strict';
 
 
-//dedication varriables
+
+//export
+import {anime} from './scripts/anime.js';
 
 
+//dedication variables
 
+
+//variables 4 MUSIC
 const buttons = document.querySelector('.buttons');
+const mainMenu = document.querySelector('.main-menu');
+const wrapperMenu = document.querySelector('.wrapper-menu');
+
+
+
+
+// variables ANIMATION
+
+const headerShowAnimation = anime({
+  targets: '.site-header',
+  height: 100 + 'vh',
+  loop: true,
+  direction: 'alternate',
+  easing: 'easeInOutCirc',
+  autoplay: false,
+  changeComplete: function(anim) {
+    anim.pause();
+  }
+});
+
+
+
+
+
 
 
 
@@ -27,12 +56,11 @@ buttons.addEventListener('click', ()=> {
 
 // button js
 
-const wrapperMenu = document.querySelector('.wrapper-menu');
 
-wrapperMenu.addEventListener('click', function(){
-  wrapperMenu.classList.toggle('open');  
+  wrapperMenu.addEventListener('click', function(){
+  wrapperMenu.classList.toggle('open'); 
+  headerShowAnimation.play(); 
+  wrapperMenu.classList.contains('open') ? mainMenu.classList.replace('main-menu__close', 'main-menu__open') :
+  mainMenu.classList.replace('main-menu__open', 'main-menu__close'); 
 });
 
-
-const whiteBtnsArray = document.querySelectorAll('.white');
-console.log(whiteBtnsArray);
